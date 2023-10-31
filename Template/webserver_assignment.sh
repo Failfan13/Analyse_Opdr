1496a3ee702fd52149b858ac8b87f14ee90f2615
OhMayJesus <99910901+OhMayJesus@users.noreply.github.com>
2023-10-31 21:18:21 +0100

    Update webserver_assignment.sh
    Added unzip/download check if exist and adding a new folder in the Install package function

--- Template/webserver_assignment.sh
+++ Template/webserver_assignment.sh
@@ -1,6 +1,8 @@
+@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Define global variable here
+config_file="config.conf"
 config_file="dev.conf"
 install_dir=""
 
@@ -8,28 +10,17 @@ install_dir=""
 declare -a dependancies=("unzip" "wget" "curl")
 
 # Added necessary packages
+declare -a packages=("")
 declare -a packages=("nosecrets" "pywebserver")
 
 # Error handling function with 2 passable arguments
 function handle_error() {
-    # Do not remove next line!
-    echo "function handle_error"
-
-    #Display the given error code
-    echo -e "Error: $1"
-
-    #On error executable
-    eval "$2"
-
-    #Exit script
-    exit 1
-}
- 
-# Function to solve dependencies
-function setup() {
+@ -29,29 +30,54 @@ function setup() {
     # Do not remove next line!
     echo "function setup"
     
+# Checks each dependency in $dependancies
+    for pkg in "${dependancies[@]}"; do
     # Checks each dependency in $dependancies
     for pkg in "${dependancies[@]}";
     do
@@ -37,6 +28,11 @@ function setup() {
         check_dependency "$pkg"
     done
 
+    # TODO check if nessassary dependecies and folder structure exists and 
+    # print the outcome for each checking step
+    
+    # TODO installation from online package requires values for
+    # package_name package_url install_dir
     # Set install directory to conf specified and use cut to remove variable name
     install_dir="$(grep -E "INSTALL_DIR=" "$config_file" | cut -d= -f2)"
 
@@ -62,6 +58,7 @@ function install_package() {
     # Do not remove next line!
     echo "function install_package"
 
+    setup
     pgk_name="$1"
     pkg_url="$2"
 
@@ -80,154 +77,63 @@ function install_package() {
     fi
 
     # TODO The logic for downloading from a URL and unizpping the downloaded files of different applications must be generic
+    echo "Downloading $pgk_name"
+    powershell -Command "Invoke-WebRequest $pkg_url -Outfile $pgk_name.exe name"
+    echo "Done!"
+    echo "Unzipping $pgk_name.."
+    
 
-    # TODO Specific actions that need to be taken for a specific application during this process should be handeld in a separate if-else
-
-    # TODO Every intermediate steps need to be handeld carefully. error handeling should be dealt with using handle_error() and/or rolleback()
-
-    # TODO If a file is downloaded but cannot be zipped a rollback is needed to be able to start from scratch
+@ -63,7 +89,7 @@ function install_package() {
     # for example: package names and urls that are needed are passed or extracted from the config file
 
     # TODO check if the application-folder and the url of the dependency exist
-
+    if $pkg_url %errorlevel% ==0 (
+    echo "Done.."
+    ) else (
+    echo "Not Done...!"
+    )
+    if $pgk_name %errorlevel% ==0 (
+    echo "Done.."
+    ) else (
+    echo "Not Done...!"
+    )
+    # TODO create a specific installation folder for the current package
+    powershell md Install
 
     # TODO Download and unzip the package
+    powershell "Expand-Archive $pgk_name -DestinationPath C:\Users\Roggerio\Install"
     # if a a problem occur during the this proces use the function handle_error() to print a messgage and handle the error
-
-    # TODO extract the package to the installation folder and store it into a dedicated folder
-    # If a problem occur during the this proces use the function handle_error() to print a messgage and handle the error
-
-    # TODO this section can be used to implement application specifc logic
-    # nosecrets might have additional commands that needs to be executed
-    # make sure the user is allowed to remove this folder during uninstall
-
-}
-
-function rollback_nosecrets() {
-    # Do not remove next line!
-    echo "function rollback_nosecrets"
-
-    # TODO rollback intermiediate steps when installation fails
-}
-
-function rollback_pywebserver() {
-    # Do not remove next line!
-    echo "function rollback_pywebserver"
-
-    # TODO rollback intermiediate steps when installation fails
-}
-
-function test_nosecrets() {
-    # Do not remove next line!
-    echo "function test_nosecrets"
-
-    # TODO test nosecrets
-    # kill this webserver process after it has finished its job
-
-}
-
-function test_pywebserver() {
-    # Do not remove next line!
-    echo "function test_pywebserver"    
-
-    # TODO test the webserver
-    # server and port number must be extracted from config.conf
-    # test data must be read from test.json  
-    # kill this webserver process after it has finished its job
-
-}
-
-function uninstall_nosecrets() {
-    # Do not remove next line!
-    echo "function uninstall_nosecrets"  
-
-    #TODO uninstall nosecrets application
-}
-
-function uninstall_pywebserver() {
-    echo "function uninstall_pywebserver"    
-    #TODO uninstall pywebserver application
-}
-
-#TODO removing installed dependency during setup() and restoring the folder structure to original state
-function remove() {
-    # Do not remove next line!
-    echo "function remove"
-
-    # Remove each package that was installed during setup
-
-}
-
-function main() {
-    # Do not remove next line!
-    echo "function main"
-
-# Verify supplied commands is 1 atleast
-    if [ $# -lt 1 ]; then
-        handle_error "No command supplied"
-    fi
-
-# Command and action given at excecution of script
-    command="$1"
-    action="$2"
-
-# Format command(up/low) and action in particular style
-    command_formatted_up="${command^^}"
-    command_formatted_low="${command,,}"
-    action_formatted="${action^^}"
-
-# Verify command is available and run attachted function
-    case $command_formatted_up in
-        "SETUP")
-            setup
-            ;;
-        "NOSECRETS" | "PYWEBSERVER")
-# Add options for "install, uninstall and test"
-            if [ "$action_formatted" = "--INSTALL" ];then
-                echo $command_formatted_low"--install"
-            elif [ "$action_formatted" = "--UNINSTALL" ]; then
-                echo $command_formatted_low"--uninstall"
-            elif [ "$action_formatted" = "--TEST" ]; then
-                echo $command_formatted_low"--test"
-            else
-                handle_error "Could not find accompanied action\nexiting..."
+@ -168,7 +194,7 @@ function main() {
             fi
             ;;
         "REMOVE")
+                remove
             remove
             ;;
         *)    
             handle_error "Could not find accompanied command\nexiting..."
-        ;;
-    esac
-
-    # Execute the appropriate command based on the arguments
-    # TODO In case of setup
-    # excute the function check_dependency and provide necessary arguments
-    # expected arguments are the installation directory specified in dev.conf
-}
-
-# Dependency checker arguments "package_name", "(true/false) auto install"
+@ -185,16 +211,17 @@ function main() {
 function check_dependency() {
     # Do not remove next line!
     echo "function check_dependency"
+# Find dependency by name in installed packages and save the Status
 
     # Find dependency by name in installed packages and save the Status
     installed=$(dpkg-query -W -f='${Status}' "$1")
 
+# If status installed found and return, otherwise provide installation instructions
     # If status installed found and return, otherwise provide installation instructions
     if [ "$installed" = "install ok installed" ]; then
         echo -e "Found and installed\n"
         return
     fi
 
+# Recursion for checking installed dependency
     # Recursion for checking installed dependency
     echo -e "Missing dependency, trying to install\n"
     if [ "$2" != false ]; then
         "$(sudo apt-get install "$1")"
-        check_dependency "$1" false
-    else
-        handle_error "$1 not found/installed, To install please use: sudo apt install $1\n"
+@ -204,39 +231,47 @@
     fi
 }
 
@@ -237,9 +143,20 @@ function folder_structure()
     # Do not remove next line!
     echo "function folder_structure"
 
+# Checking the file & folder structure
     # Checking the file & folder structure
     echo -e "Checking folder structure\n"
 
+
+    echo "Checking apps folder"
+    if [ ! -d "./apps/" ]; then
+        echo "Creating apps folder"
+        mkdir ./apps/
+    else
+        echo "Verified apps folder"
+    fi
+
+    echo "Checking dev.conf"
     echo "Checking config file"
     if [ ! -f "dev.conf" ]; then
         handle_error "Missing dev.conf file"
@@ -247,6 +164,7 @@ function folder_structure()
         echo "Verified dev.conf"
     fi
 
+    echo "Checking test.json"
     echo "Checking test file"
     if [ ! -f "test.json" ]; then
         handle_error "Missing test.json"
